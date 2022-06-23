89c89dce25afef40220d8dfc13f89c062115ebfa
Uyoyou Uwuseba <uwusebauyoyou@gmail.com>
2022-06-23 04:33:21 +0100

    feat: add LP tokens and Exchange::removeLiquidity function
    Just like the actual UniswapV1, liquidity providers are incentivized
    to provide their funds via fees accrued on each swap performed on
    Uniswap. Fees are tracked, collected and distributed pro-rata against
    LP tokens. LP tokens are a proof of liqudity provided and fees acrued
    against that liquidity.

--- contracts/exchange.sol
+++ contracts/exchange.sol
@@ -1,19 +1,28 @@
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract Exchange {
+contract Exchange is ERC20 {
     address public tokenAddress;
 
-    constructor(address _token) {
+    constructor(address _token) ERC20("Uniswap-V1", "Uni-v1") {
         require(_token != address(0), "invlaid token address");
         tokenAddress = _token;
     }
 
-    function addLiquidity(uint256 _tokenAmount) public payable {
+    function addLiquidity(uint256 _tokenAmount)
+        public
+        payable
+        returns (uint256)
+    {
         if (getReserve() == 0) {
             IERC20 token = IERC20(tokenAddress);
             token.transferFrom(msg.sender, address(this), _tokenAmount);
+
+            uint256 liquidity = address(this).balance;
+            _mint(msg.sender, liquidity);
+            return liquidity;
         } else {
             uint256 ethReserve = address(this).balance - msg.value;
             uint256 tokenReserve = getReserve();
@@ -22,6 +31,10 @@ contract Exchange {
 
             IERC20 token = IERC20(tokenAddress);
             token.transferFrom(msg.sender, address(this), tokenAmount);
+
+            uint256 liquidity = (totalSupply() * msg.value) / ethReserve;
+            _mint(msg.sender, liquidity);
+            return liquidity;
         }
     }
 
@@ -45,7 +58,10 @@ contract Exchange {
     ) private pure returns (uint256) {
         require(inputReserve > 0 && outputReserve > 0, "invalid reserves");
 
-        return (inputAmount * outputReserve) / (inputReserve + inputAmount);
+        uint256 inputAmountWithFee = inputAmount * 99;
+        uint256 numerator = inputAmountWithFee * outputReserve;
+        uint256 denomicator = (inputReserve * 100) + inputAmountWithFee;
+        return numerator / denomicator;
     }
 
     function getTokenAmount(uint256 _ethSold) public view returns (uint256) {
@@ -88,5 +104,21 @@ contract Exchange {
             _tokensSold
         );
         payable(msg.sender).transfer(ethBought);
+    }
+
+    function removeLiquidity(uint256 _amount)
+        public
+        returns (uint256, uint256)
+    {
+        require(_amount > 0, "invalid amount");
+
+        uint256 ethAmount = (address(this).balance * _amount) / totalSupply();
+        uint256 tokenAmount = (getReserve() * _amount) / totalSupply();
+
+        _burn(msg.sender, _amount);
+        payable(msg.sender).transfer(ethAmount);
+        IERC20(tokenAddress).transfer(msg.sender, tokenAmount);
+
+        return (ethAmount, tokenAmount);
     }
 }
